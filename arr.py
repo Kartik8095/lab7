3b9f78610fa1f724fd455fa954d937fa3dd3f7f3
Kartik8095 <02fe24bca025@kletech.ac.in>
2026-01-02 19:29:38 +0530

    Implement main function to calculate average scores
    Fix syntax errors and calculate average of scores.

---
+++ arr.py
@@ -0,0 +1,9 @@
+def main(){
+  scores=[10,20,30,50]
+total=sum(scores)
+avearage=total/len(scores)
+print("=== main/master branch uotput")
+print("scores={scores}")
+print("avearage={avearage}")
+if__name__=="__main__":
+main()
