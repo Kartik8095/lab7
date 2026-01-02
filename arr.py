9d22711d4b7c655222516b0fde5ab5e1a921dc69
Kartik8095 <02fe24bca025@kletech.ac.in>
2026-01-02 19:36:06 +0530

    Fix syntax errors and improve print statements

--- arr.py
+++ arr.py
@@ -1,9 +1,9 @@
-def main(){
+def main():
   scores=[10,20,30,50]
 total=sum(scores)
 avearage=total/len(scores)
-print("=== main/master branch uotput")
-print("scores={scores}")
-print("avearage={avearage}")
+print(f"=== main/master branch uotput")
+print(f"scores={scores}")
+print(f"avearage={avearage}")
 if__name__=="__main__":
 main()
