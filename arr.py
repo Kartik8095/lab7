b6c1f8779d1e22017b98ebef4dfc4f1782b5e39c
Kartik8095 <02fe24bca025@kletech.ac.in>
2026-01-02 19:38:47 +0530

    Fix indentation and spelling errors in arr.py

--- arr.py
+++ arr.py
@@ -1,9 +1,11 @@
 def main():
-  scores=[10,20,30,50]
-total=sum(scores)
-avearage=total/len(scores)
-print(f"=== main/master branch uotput")
-print(f"scores={scores}")
-print(f"avearage={avearage}")
-if__name__=="__main__":
-main()
+    scores = [10, 20, 30, 50]
+    total = sum(scores)
+    average = total / len(scores)
+
+    print("=== main/master branch output ===")
+    print(f"Scores = {scores}")
+    print(f"Average = {average}")
+
+if __name__ == "__main__":
+    main()
